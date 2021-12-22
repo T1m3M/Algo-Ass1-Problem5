893fb8ca184ce919f6bd90edcb7ea3b31d5630b7
T1m3-m4ch1n3 <abdelrahman_nasr99@hotmail.com>
2021-12-22 08:08:55 +0200

    Adding test cases in Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
 public class Main {
 
     public static void main(String[] args) {
+        SkipList mySkipList = new SkipList();
 
+        // inserting test data
+        mySkipList.insert(2); mySkipList.insert(10); mySkipList.insert(15);
+        mySkipList.insert(16); mySkipList.insert(31); mySkipList.insert(71);
+        mySkipList.insert(86); mySkipList.insert(89); mySkipList.insert(91);
+        mySkipList.insert(96);
+
+        System.out.println("Number of Layers = " + mySkipList.getLayers());
+        System.out.print("\n");
+
+        for(int i = mySkipList.getLayers() - 1; i >= 0; i--)
+            mySkipList.printLayer(i);
+        System.out.print("\n");
+
+        // deleting an existing number
+        mySkipList.delete(91);
+
+        for(int i = mySkipList.getLayers() - 1; i >= 0; i--)
+            mySkipList.printLayer(i);
+        System.out.print("\n");
+
+        // deleting a non-existing number
+        mySkipList.delete(101);
+
+        for(int i = mySkipList.getLayers() - 1; i >= 0; i--)
+            mySkipList.printLayer(i);
+        System.out.print("\n");
     }
 }
